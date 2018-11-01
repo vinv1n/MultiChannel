782b8d544c71b807043e775ff9132101dd9a6cc6
vinv1n <jusipola@student.oulu.fi>
2018-11-01 14:39:14 +0200

    Add handler for mongo

--- app/database/db.py
+++ app/database/db.py
@@ -5,14 +5,19 @@ class Mongo:
     """
     Class for handling database
     """
-    def __init__(self, database):
+    def __init__(self, database_name):
         # FIXME change localhost to something else
         self.client = pymongo.MongoClient(host="localhost", port=27017)
 
-        self.database = self._create_database(database)
+        #Default collections
+        self.user_collection = None
+        self.message_collection = None
+
+        self.database = self._create_database(database_name)
         if self.database is None:
             raise Exception("Failure in database creation")
 
+        self._create_default_collections()
 
     def _create_database(self, name):
         try:
@@ -21,9 +26,13 @@ class Mongo:
         except Exception:
             return None
 
-    def create_collection(self, collection_name):
+    def create_new_collection(self, collection_name):
         collection = self.database[collection_name]
         return collection
 
     def get_collections(self):
         return self.database.list_collection_names()
+
+    def _create_default_collections(self):
+        self.user_collection = self.database['users']
+        self.message_collection = self.database['messages']
