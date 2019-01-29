ee13d874e5c4b5859ab319a005a2026f6af8546d
vinv1n <jusipola@student.oulu.fi>
2019-01-29 23:18:38 +0200

    Add irc

--- app/channels/irc_handler.py
+++ app/channels/irc_handler.py
@@ -2,15 +2,16 @@
 Message handler for IRC api
 """
 import logging
-from utils import Message, Networking
+import requests
 
 logger = logging.getLogger(__name__)
 
-networking = Networking()  # class that handles requests
 class IRC:
 
-    @staticmethod
-    def send_message(body, message_type, group_message, user, channel_information):
+    def __init__(self, database):
+        self.db = database
+
+    def send_message(self, message, user, info):
         """
         :param message: instance of message class
         :return: True if message was send succesfully, otherwise False
@@ -19,10 +20,25 @@ class IRC:
         # TODO hide this
         # url to irc endpoint
         url = "127.0.0.1:8000/send/"
-        message = Message(body, message_type, group_message, user, channel_information)
+        rc = message.get("receivers")
 
-        response = networking.make_post_request(headers=message.as_dict(), url=url)
-        if not response:
+        result = []
+        for user in rc:
+            user_info = self.db.get_user(user)
+            nick = user_info.get("channels").get("irc").get("nickname")
+            if not nick:
+                continue
+
+            data = {
+                "receiver": nick,
+                "message_id": message.get("id"),
+                "message": message.get("message")
+            }
+
+            response = requests.post(url, data=data)
+            result.append(response.json())
+
+        if not result:
             return False
 
         return True
