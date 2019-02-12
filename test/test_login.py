b14a45b849c253fe00bc42acbaa98fa8481e6932
Timo Mattila <timattil@student.oulu.fi>
2019-02-12 19:48:32 +0200

    Fix test_login URLs

--- test/test_login.py
+++ test/test_login.py
@@ -5,6 +5,7 @@ import argparse
 import logging
 import json
 
+URL = 'http://127.0.0.1:5000/api'
 
 class Login_test(unittest.TestCase):
 
@@ -15,68 +16,68 @@ class Login_test(unittest.TestCase):
     def test_login_success(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 200)
 
     def test_login_bad_username (self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "bad_username", "password": "admin"}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 401)
     
     def test_login_bad_password(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "wrongpassword"}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 401)
 
     def test_login_empty_fields(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "", "password": ""}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 401)
     
     def test_login_missing_password(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", }
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 400)
     
     def test_login_missing_username(self):
         headers = {'Content-type': 'application/json'}
         data = {"password": "admin", }
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 400)
 
     def test_login_extra_field(self):
         headers = {'Content-type': 'application/json'}
         data = {"password": "admin", "username" : "admin", "extra" : "extra" }
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertEqual(response.status_code, 400)
 
     def test_login_missing_data(self):
         headers = {'Content-type': 'application/json'}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers)
+        response = requests.post('{}/user-login'.format(URL), headers=headers)
 
         self.assertEqual(response.status_code, 400)
 
     def test_login_bad_header(self):
         data = {"username": "admin", "password": "admin"}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), data=json.dumps(data))
 
         self.assertEqual(response.status_code, 400)
     
     def test_return_cookies(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         self.assertTrue(response.cookies.get('access_token_cookie'))
         self.assertTrue(response.cookies.get('refresh_token_cookie'))
@@ -89,9 +90,9 @@ class Logout_test(unittest.TestCase):
     def test_logout(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        login_response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        login_response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
-        response = requests.post('http://0.0.0.0:5000/api/logout',
+        response = requests.post('{}/logout'.format(URL),
         cookies={'access_token_cookie':login_response.cookies.get('access_token_cookie'),
          'refresh_token_cookie':login_response.cookies.get('refresh_token_cookie')})
 
@@ -100,9 +101,9 @@ class Logout_test(unittest.TestCase):
     def test_logout_no_token(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        login_response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        login_response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
-        response = requests.post('http://0.0.0.0:5000/api/logout')
+        response = requests.post('{}/logout'.format(URL))
 
         self.assertEqual(response.status_code, 401)
 
@@ -114,7 +115,7 @@ class LoginRefresh_test(unittest.TestCase):
 
     def test_refresh_no_cookies(self):
 
-        response = requests.post('http://0.0.0.0:5000/api/re-login')
+        response = requests.post('{}/re-login'.format(URL))
 
         self.assertEqual(response.status_code, 401)
     
@@ -122,10 +123,10 @@ class LoginRefresh_test(unittest.TestCase):
     def test_refresh(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        login_response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        login_response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
         
-        response = requests.post('http://0.0.0.0:5000/api/re-login',
+        response = requests.post('{}/re-login'.format(URL),
          cookies={'access_token_cookie':login_response.cookies.get('access_token_cookie'),
          'refresh_token_cookie':login_response.cookies.get('refresh_token_cookie')})
 
@@ -141,9 +142,9 @@ class LogoutRefresh_test(unittest.TestCase):
     def test_logoutRefresh(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        login_response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        login_response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
-        response = requests.post('http://0.0.0.0:5000/api/re-logout',
+        response = requests.post('{}/re-logout'.format(URL),
         cookies={'access_token_cookie':login_response.cookies.get('access_token_cookie'),
          'refresh_token_cookie':login_response.cookies.get('refresh_token_cookie')})
 
@@ -152,9 +153,9 @@ class LogoutRefresh_test(unittest.TestCase):
     def test_logoutRefresh_no_token(self):
         headers = {'Content-type': 'application/json'}
         data = {"username": "admin", "password": "admin"}
-        login_response = requests.post('http://0.0.0.0:5000/api/user-login', headers=headers, data=json.dumps(data))
+        login_response = requests.post('{}/user-login'.format(URL), headers=headers, data=json.dumps(data))
 
-        response = requests.post('http://0.0.0.0:5000/api/re-logout')
+        response = requests.post('{}/re-logout'.format(URL))
 
         self.assertEqual(response.status_code, 401)
 
