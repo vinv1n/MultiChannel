626150a24d6635a35e401876e7275baa1ce2494b
Timo Mattila <timattil@student.oulu.fi>
2019-03-12 14:27:51 +0200

    Cleaning up unused code

--- tokens.py
+++
@@ -1,7 +0,0 @@
-# Telegram access token
-# TODO investigate if needs to be replaced
-TELEGRAM_TOKEN = "734503972:AAH_fbUmJRypHS1ynVI9vLPcsyiFU6SgsME"
-
-# twitter access token
-TWITTER_TOKEN_PUBLIC = ""
-TWITTER_TOKEN_SECRET = ""
